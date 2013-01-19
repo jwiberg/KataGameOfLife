f41b8c69120999929e4d911e4dd6e0d425a8f2b5
Juha Wiberg <juha.wiberg@gmail.com>
2013-01-19 16:00:12 +0200

    First commit, adding testclass

---
+++ src/GameOfLifeTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * User: Juha
+ * Date: 19.1.2013
+ * Time: 15:40
+ */
+
+public class GameOfLifeTest {
+
+    @Test
+    public void doesThisWork() {
+
+    }
+
+    @Test
+    public void doesThisSecondWork() {
+
+    }
+
+}
