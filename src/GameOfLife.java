d564d58f17f7b533747f2dc56da2a9104e305b1b
Juha Wiberg <juha.wiberg@gmail.com>
2013-01-20 19:59:03 +0200

    Simple special cases work, but out commented tests gonna lead this implementation different direction... :)

---
+++ src/GameOfLife.java
@@ -0,0 +1,8 @@
+/**
+ * User: Juha
+ * Date: 20.1.2013
+ * Time: 19:35
+ */
+public class GameOfLife {
+
+}
--- src/GameOfLifeTest.java
+++ src/GameOfLifeTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * User: Juha
  * Date: 19.1.2013
@@ -9,13 +11,54 @@ import org.junit.Test;
 public class GameOfLifeTest {
 
     @Test
-    public void doesThisWork() {
-
+    public void zeroLiveCellsShouldGenerateZeroLiveCells() {
+        String[][] oldGeneration = {};
+        String[][] newGeneration = generateNextGeneration(oldGeneration);
+        assertArrayEquals(new String[0][0], newGeneration);
     }
 
     @Test
-    public void doesThisSecondWork() {
+    public void oneLiveCellsWithoutNeighboursShouldGenerateNoLiveCells() {
+        String[][] oldGeneration = {{"*"}};
+        String[][] newGeneration = generateNextGeneration(oldGeneration);
+        assertArrayEquals(new String[][]{{"."}}, newGeneration);
+    }
 
+   @Test
+    public void twoLiveCellsReturnsNoLiveCells() {
+        String[][] oldGeneration = {{"*","*"}};
+        String[][] newGeneration = generateNextGeneration(oldGeneration);
+        assertArrayEquals(new String[][]{{".","."}}, newGeneration);
+    }
+
+/*
+    @Test
+    public void oneLiveCellIn2x2GridShouldGenerateNoLiveCells() {
+        String[][] oldGeneration = {{"*","."},{".","."}};
+        String[][] newGeneration = generateNextGeneration(oldGeneration);
+        assertArrayEquals(new String[][]{{".","."},{".","."}}, newGeneration);
+    }
+
+    @Test
+    public void twoLiveCellIn2x2GridShouldGenerateNoLiveCells() {
+        String[][] oldGeneration = {{"*","."},{".","*"}};
+        String[][] newGeneration = generateNextGeneration(oldGeneration);
+        assertArrayEquals(new String[][]{{".","."},{".","."}}, newGeneration);
+    }
+*/
+
+    private String[][] generateNextGeneration(String[][] oldGeneration) {
+        String[][] newGeneration = null;
+        //Couple of special cases
+        if(oldGeneration.length == 0) {
+            newGeneration = new String[oldGeneration.length][0];
+        }else if(oldGeneration.length == 1 && oldGeneration[0].length < 3) {
+            newGeneration = new String[oldGeneration.length][oldGeneration[0].length];
+            for(int i = 0; i < oldGeneration[0].length; i++) {
+                newGeneration[0][i] = ".";
+            }
+        }
+        return newGeneration;
     }
 
 }
